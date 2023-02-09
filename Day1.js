467b8f81feab1a1cc02063dfca44e57f7a0c42ac
Utpana <89650330+Utpana98Marwah@users.noreply.github.com>
2023-02-09 10:22:52 +0530

    Update Day1.js

--- Day1.js
+++ Day1.js
@@ -1,5 +1,5 @@
 //First, print each vowel in  on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in .
-//Second, print each consonant (i.e., non-vowel) in  on a new line in the same order as it appeared in .
+//Second, print each consonant (i.e., non-vowel) in  on a new line in the same order as it appeared in.
 function vowelsAndConsonants(arr) {
      arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     let vowel = [];
