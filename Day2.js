f819251cc946567202f8ec95f8e0cefaab96e7b5
Utpana <89650330+Utpana98Marwah@users.noreply.github.com>
2023-02-09 10:34:34 +0530

    Update Day2.js

--- Day2.js
+++ Day2.js
@@ -1,4 +1,6 @@
+//---------------------------------------Q.1--------------------------------------
 // Get  largest number
+
 let nums = [2, 3, 6, 6, 4,5];
 let largest = 0;
     for(let i of nums)
@@ -9,11 +11,27 @@ let largest = 0;
         }
     }
 console.log(largest);
-//------------------------------------------------------------------
+
+
+//--------------------------------------------Q.2--------------------------------------
 //transformToObjects()
 //[{val: 1}, {val: 2}, {val: 3}]
+
 let numbers = [1, 2, 3];
 let result = numbers.map(function(num){
     return {val:num}
 });
 console.log(result);
+
+
+//----------------------------------------------Q.3--------------------------------------
+//Remove duplicate values from array
+
+let arr = [1,2,7,9,6,4,2,9];
+let uniqueValues = [];
+for(let i of arr){
+  if(!uniqueValues.includes(i)){
+    uniqueValues.push(i);
+  }
+}
+console.log(uniqueValues.sort());
